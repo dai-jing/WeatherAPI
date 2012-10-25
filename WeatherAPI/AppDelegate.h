abbd7ddfae22ca2a475424220138a4794cb89b80
Kobe Dai <kobehebe24@Jing-Dais-MacBook-Pro.local>
2012-10-25 17:34:28 -0500

    Initial Commit

---
+++ WeatherAPI/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  WeatherAPI
+//
+//  Created by Kobe Dai on 10/25/12.
+//  Copyright (c) 2012 Kobe Dai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ WeatherAPI/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  WeatherAPI
+//
+//  Created by Kobe Dai on 10/25/12.
+//  Copyright (c) 2012 Kobe Dai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
