3f32404cc35cc344f8b9b6c0e70672ef1d7be19b
819419423 <daijing24@gmail.com>
2012-11-01 20:24:16 -0500

    Update WeatherAPI/ViewController.m

--- WeatherAPI/ViewController.m
+++ WeatherAPI/ViewController.m
@@ -3,12 +3,17 @@
 //  WeatherAPI
 //
 //  Created by Kobe Dai on 10/25/12.
-//  Copyright (c) 2012 Kobe Dai. All rights reserved.
+//  Copyright (c) 2012 Jing Dai. All rights reserved.
 //
 
 #import "ViewController.h"
 
 @interface ViewController ()
+{
+    NSMutableData *responseData;
+    NSURL *theURL;
+    NSString *zipCode;
+}
 
 @end
 
@@ -24,6 +29,74 @@
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)queryService:(id)sender
+{
+    NSString *url = [NSString stringWithFormat: @"http://api.wunderground.com/api/bb4adda5d3b86ec4/conditions/q/%@.json", zipCode];
+    theURL = [NSURL URLWithString: url];
+    NSURLRequest *request = [[NSURLRequest alloc] initWithURL: theURL];
+    NSURLConnection *connection = [[NSURLConnection alloc] initWithRequest: request delegate: self];
+    if (connection)
+    {
+        responseData = [[NSMutableData alloc] init];
+    }
+    else
+    {
+        NSLog(@"Failed");
+    }
+}
+
+- (BOOL)textFieldShouldReturn:(UITextField *)textField
+{
+    zipCode = [textField text];
+    [textField resignFirstResponder];
+    return YES;
+}
+
+#pragma mark NSURLConnection Data Delegate Methods
+
+- (void)connection: (NSURLConnection *)connection didReceiveResponse:(NSURLResponse *)response
+{
+    [responseData setLength: 0];
+}
+
+- (void)connection: (NSURLConnection *)connection didReceiveData:(NSData *)data
+{
+    [responseData appendData: data];
+}
+
+- (void)connectionDidFinishLoading: (NSURLConnection *)connection
+{
+    // Insert code to parse the content here...
+    NSError *myError = nil;
+    NSDictionary *res = [NSJSONSerialization JSONObjectWithData: responseData options: NSJSONReadingMutableLeaves error: &myError];
+    NSArray *results = [res objectForKey: @"current_observation"];
+    NSArray *cur = [results valueForKey: @"weather"];
+    NSArray *tmp = [results valueForKey: @"temp_c"];
+    
+    if (cur == nil || tmp == nil)
+    {
+        UIAlertView *alert = [[UIAlertView alloc]
+                              initWithTitle: @"Errors"
+                              message: @"Please check your zip code"
+                              delegate: self
+                              cancelButtonTitle: nil
+                              otherButtonTitles: @"OK", nil];
+        [alert show];
+    }
+    else
+    {
+        NSString *message = [NSString stringWithFormat: @"Current Weather Conditions: %@, Current Tmp: %@", cur, tmp];
+    
+        UIAlertView *alert = [[UIAlertView alloc]
+                          initWithTitle: @"This is your local weather condition"
+                          message: message
+                          delegate: self
+                          cancelButtonTitle: nil
+                          otherButtonTitles: @"OK", nil];
+        [alert show];
+    }
 }
 
 @end
